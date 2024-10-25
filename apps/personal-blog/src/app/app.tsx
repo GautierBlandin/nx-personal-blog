df7ccb7381bd69a763d64a5c6092cf2615711247
Gautier <gautier.blandin@gmail.com>
2024-10-25 17:11:14 +0200

    feat: remove useless root

--- apps/personal-blog/src/app/app.tsx
+++ apps/personal-blog/src/app/app.tsx
@@ -1,15 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './home';
 import { DeployRemixViteOnLambda } from '@nx-personal-blog/web-articles';
-import { Root } from './root';
 
 export function App() {
   return (
     <Routes>
-      <Route element={<Root />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/articles/deploy-remix-vite-on-lambda-using-pulumi" element={<DeployRemixViteOnLambda />} />
-      </Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/articles/deploy-remix-vite-on-lambda-using-pulumi" element={<DeployRemixViteOnLambda />} />
     </Routes>
   );
 }
