bbf39beb639688712cf2ed71b985e92ae552e2c0
Gautier <gautier.blandin@gmail.com>
2024-10-25 15:34:05 +0200

    feat: fix root layout

--- apps/personal-blog/src/app/app.tsx
+++ apps/personal-blog/src/app/app.tsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './home';
 import { DeployRemixViteOnLambda } from '@nx-personal-blog/web-articles';
+import { Root } from './root';
 
 export function App() {
   return (
-    <div>
-      <Routes>
+    <Routes>
+      <Route element={<Root />}>
         <Route path="/" element={<Home />} />
         <Route path="/articles/deploy-remix-vite-on-lambda-using-pulumi" element={<DeployRemixViteOnLambda />} />
-      </Routes>
-    </div>
+      </Route>
+    </Routes>
   );
 }
---
+++ apps/personal-blog/src/app/root.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import '../styles.css';
+
+export function Root() {
+  return (
+    <html lang="en">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cactus+Classical+Serif&display=swap" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap" />
+      <link rel="icon" href="/favicon.ico" />
+    </Helmet>
+    <body>
+    <Outlet />
+    </body>
+    </html>
+  );
+}
