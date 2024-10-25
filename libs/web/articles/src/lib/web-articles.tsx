ca75db3ca6667a0ca08b9c97ab1d9469435ca507
Gautier <gautier.blandin@gmail.com>
2024-10-25 15:31:30 +0200

    prism: improve imports

--- libs/web/articles/src/lib/web-articles.tsx
+++ libs/web/articles/src/lib/web-articles.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 import 'prismjs/themes/prism-okaidia.css';
-import 'prismjs/components/prism-typescript';
-import 'prismjs/components/prism-bash';
-import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-typescript.js';
+import 'prismjs/components/prism-bash.js';
+import 'prismjs/components/prism-json.js';
 import 'prismjs/plugins/toolbar/prism-toolbar.min.css';
-import 'prismjs/plugins/toolbar/prism-toolbar.min';
-import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min';
+import 'prismjs/plugins/toolbar/prism-toolbar.min.js';
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js';
 
 export const Article = ({ articleLink }: { articleLink: string }) => {
   const [content, setContent] = useState<string>('');
