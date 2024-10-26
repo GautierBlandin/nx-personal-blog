116e44d9a6846a95c59b6f0fdc94c7d59bba49b2
Gautier <gautier.blandin@gmail.com>
2024-10-26 23:53:20 +0200

    feat: remove useless command

--- libs/web/articles/src/lib/Markdown/config.tsx
+++ libs/web/articles/src/lib/Markdown/config.tsx
@@ -1,5 +1,3 @@
-// Dev/context-gpt/libs/shared/ui/src/lib/Markdown/config.tsx
-
 import { Config } from '@markdoc/markdoc';
 import { fence } from './code-block';
 import { paragraph } from './paragraph';
