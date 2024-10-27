0c9a96edf36521d6e4c1c1fcc7ebcd98ec07b889
Gautier <gautier.blandin@gmail.com>
2024-10-27 12:47:05 +0100

    feat: update paragraph styling

--- libs/web/articles/src/lib/Markdown/paragraph.tsx
+++ libs/web/articles/src/lib/Markdown/paragraph.tsx
@@ -1,7 +1,7 @@
 import { cn } from '../cn';
 
 export const Paragraph = ({ children, className }: { children: React.ReactNode; className?: string }) => {
-  return <p className={cn(className)}>{children}</p>;
+  return <p className={cn('font-paragraph', className)}>{children}</p>;
 };
 
 export const paragraph = {
