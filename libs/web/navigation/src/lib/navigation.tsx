59e1b918d82dee8af6397787f1ac7d3d93e1f9af
Gautier <gautier.blandin@gmail.com>
2024-10-27 21:02:50 +0100

    feat: add navigation library

---
+++ libs/web/navigation/src/lib/navigation.tsx
@@ -0,0 +1,15 @@
+import styled from 'tailwind';
+
+const StyledNavigation = styled.div`
+  color: pink;
+`;
+
+export function Navigation() {
+  return (
+    <StyledNavigation>
+      <h1>Welcome to Navigation!</h1>
+    </StyledNavigation>
+  );
+}
+
+export default Navigation;
