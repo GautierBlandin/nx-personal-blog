1251e2b395dc1598bf111beb4d3c7c6a3fc4bbeb
Gautier <gautier.blandin@gmail.com>
2024-10-27 23:01:46 +0100

    feat: improve styling of articles so that they have a minimum margin

--- libs/web/articles/src/lib/ArticlePage.tsx
+++ libs/web/articles/src/lib/ArticlePage.tsx
@@ -4,12 +4,14 @@ import { WithNavbar } from '@nx-personal-blog/navigation';
 
 const ArticlePageContent = ({ articleLink, title, description }: { articleLink: string, title: string, description: string }) => {
   return (
-    <div className="max-w-3xl mx-auto">
-      <Helmet>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-      </Helmet>
-      <Article articleLink={articleLink} />
+    <div className="flex justify-center mx-4">
+      <div className="max-w-3xl w-full">
+        <Helmet>
+          <title>{title}</title>
+          <meta name="description" content={description} />
+        </Helmet>
+        <Article articleLink={articleLink} />
+      </div>
     </div>
   );
 };
