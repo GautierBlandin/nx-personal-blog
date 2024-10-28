98367045e13cf1c7dee067c8b19e99abe1222820
Gautier <gautier.blandin@gmail.com>
2024-10-28 15:38:45 +0100

    feat: update main route with articles configuration

--- apps/personal-blog/src/app/app.tsx
+++ apps/personal-blog/src/app/app.tsx
@@ -1,6 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './home';
-import { ArticlePage, articleRoute, articles, articleToArticlePageProps } from '@nx-personal-blog/web-articles';
+import {
+  ArticlePage,
+  articleRoute,
+  articles,
+  articleToArticlePageProps
+} from '@nx-personal-blog/web-articles';
 
 export function App() {
 
@@ -8,9 +13,10 @@ export function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path={articleRoute(articles.deployRemixViteOnAwsWithPulumi.identifier)} element={<ArticlePage
-        {...articleToArticlePageProps(articles.deployRemixViteOnAwsWithPulumi)}
-      />} />
+      {...Object.values(articles).map((article) =>
+        <Route path={articleRoute(article.identifier)} element={<ArticlePage
+        {...articleToArticlePageProps(article)}
+        />} />)}
       <Route path="/articles" element={<ArticlePage
         articleLink="/content/articles.md"
         title="Gautier Blandin - A blog about software engineering"
