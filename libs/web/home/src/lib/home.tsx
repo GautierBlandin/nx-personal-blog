4ad4653f615e551b6d698e91debabb3086c3066f
Gautier <gautier.blandin@gmail.com>
2024-10-30 08:04:06 +0100

    feat: add home library

---
+++ libs/web/home/src/lib/home.tsx
@@ -0,0 +1,14 @@
+export function Home() {
+  return (
+    <div>
+      <p>
+        Hey, I'm Gautier, and I love Building things!
+      </p>
+      <p>
+        I'm a software engineer at [TrackIt](https://trackit.io/), where I work on full-stack web development
+        and cloud stuff with AWS.
+      </p>
+      <h2> Recent articles </h2>
+    </div>
+  );
+}
