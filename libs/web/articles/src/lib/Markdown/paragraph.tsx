5c709d8cb08576d580bcc5f678ec74f3d6dac668
Gautier <gautier.blandin@gmail.com>
2024-10-30 08:04:43 +0100

    feat: move style to global style.css

--- libs/web/articles/src/lib/Markdown/paragraph.tsx
+++ libs/web/articles/src/lib/Markdown/paragraph.tsx
@@ -1,7 +1,5 @@
-import { cn } from '../cn';
-
 export const Paragraph = ({ children, className }: { children: React.ReactNode; className?: string }) => {
-  return <p className={cn('font-paragraph', className)}>{children}</p>;
+  return <p className={className}>{children}</p>;
 };
 
 export const paragraph = {
