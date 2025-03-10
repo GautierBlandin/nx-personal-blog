a6195fb4e030f54b1418faa0987aec1b6fc8d6f0
Gautier <gautier.blandin@gmail.com>
2025-03-10 23:26:34 +0100

    feat(banner): add banner shadow

--- libs/web/articles/src/lib/Banner.tsx
+++ libs/web/articles/src/lib/Banner.tsx
@@ -7,9 +7,9 @@ interface BannerProps {
 
 export const Banner: React.FC<BannerProps> = ({ title, subtitle }) => {
   return (
-    <div className="w-full bg-neutral-800 text-white py-8 px-4 mb-8">
-      <div className="container mx-auto max-w-8xl pl-8">
-        <h1 className="text-white mb-2">{title}</h1>
+    <div className="w-full bg-neutral-800 text-white py-32 mb-16 shadow-[0_6px_10px_-2px_rgba(0,0,0,0.4)]">
+      <div className="mx-auto max-w-8xl">
+        <h1 className="text-white py-0">{title}</h1>
         {subtitle && <h3 className="text-white">{subtitle}</h3>}
       </div>
     </div>
--- libs/web/articles/src/lib/article-identifiers.ts
+++ libs/web/articles/src/lib/article-identifiers.ts
@@ -1,6 +1,9 @@
+import { ArticlePageProps } from './ArticlePage';
+
 export interface Article {
   identifier: string;
   title: string;
+  subtitle?: string;
   description: string;
   date: string; // YYYY-MM-DD
 }
@@ -8,28 +11,33 @@ export interface Article {
 export const articles = {
   terraformPulumiSstTradeoffAnalsys: {
     identifier: 'terraform-pulumi-sst-tradeoff-analysis',
-    title: "Terraform vs Pulumi vs SST: A Tradeoffs analysis",
-    description: "An in-depth comparison of three popular Infrastructure as Code tools.",
+    title: 'Terraform vs Pulumi vs SST',
+    subtitle: 'A tradeoffs analysis',
+    description:
+      'An in-depth comparison of three popular Infrastructure as Code tools.',
     date: '2025-03-12',
   },
   typescriptSwitchExhaustivenessCheck: {
     identifier: 'typescript-switch-exhaustiveness-check',
     title: 'Check switch statement exhaustiveness with typescript',
-    description: 'A technical tutorial for checking switch statement exhaustiveness with typescript.',
+    description:
+      'A technical tutorial for checking switch statement exhaustiveness with typescript.',
     date: '2024-10-28',
   },
   deployRemixViteOnAwsWithPulumi: {
     identifier: 'deploy-remix-vite-on-aws-using-pulumi',
     title: 'Deploy Remix-Vite on AWS with Pulumi',
-    description: 'A technical tutorial for deploying a Remix-Vite app on AWS using Pulumi.',
+    description:
+      'A technical tutorial for deploying a Remix-Vite app on AWS using Pulumi.',
     date: '2024-06-15',
-  }
+  },
 } satisfies Record<string, Article>;
 
-export function articleToArticlePageProps(article: Article) {
+export function articleToArticlePageProps(article: Article): ArticlePageProps {
   return {
     articleLink: articleContentPath(article.identifier),
     title: article.title,
+    subtitle: article.subtitle,
     description: article.description,
   };
 }
