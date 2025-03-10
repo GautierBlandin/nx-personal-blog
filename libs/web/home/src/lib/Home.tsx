ecdd36fa4ba9c5a76745fcad0232f9459d66ea1b
Gautier <gautier.blandin@gmail.com>
2025-03-10 23:58:22 +0100

    feat(home): fix home layout

--- libs/web/home/src/lib/Home.tsx
+++ libs/web/home/src/lib/Home.tsx
@@ -1,38 +1,59 @@
 import { articleRoute, articles } from '@nx-personal-blog/web-articles';
 import { NavbarLayout } from '@nx-personal-blog/navigation';
+import React from 'react';
 
 export function Home() {
   return (
     <NavbarLayout>
-      <div>
-        <p>
-          Hey, I'm Gautier, and I love building things!
-        </p>
-        <p>
-          I'm a software engineer at <a href="https://trackit.io/">TrackIt</a>, where I work on full-stack web
-          development
-          and cloud stuff with AWS.
-        </p>
-        <hr />
-        <h5> Recent articles </h5>
-        <ul>
-          {Object.values(articles)
-            .slice(0, 5)
-            .map((article) => <li key={article.identifier}><a
-              href={articleRoute(article.identifier)}>{article.title}</a></li>)}
-        </ul>
-        <hr />
-        <h5>Socials</h5>
-        <ul>
-          <li><a href="https://github.com/gautierblandin">GitHub</a></li>
-          <li><a href="https://www.linkedin.com/in/gautier-blandin-661640138/">LinkedIn</a></li>
-        </ul>
-        <hr />
-        <h5>Contact</h5>
-        <p>
-          Feel free to reach out to me at: <a href="mailto:gautier.blandin.dev@gmail.com">gautier.blandin.dev@gmail.com</a>
-        </p>
-      </div>
+      <main className="flex-grow">
+        <div className="flex justify-center mx-4">
+          <div className="max-w-6xl w-full">
+            <div>
+              <p>Hey, I'm Gautier, and I love building things!</p>
+              <p>
+                I'm a software engineer at{' '}
+                <a href="https://trackit.io/">TrackIt</a>, where I work on
+                full-stack web development and cloud stuff with AWS.
+              </p>
+              <hr />
+              <h5> Recent articles </h5>
+              <ul>
+                {Object.values(articles)
+                  .slice(0, 5)
+                  .map((article) => (
+                    <li key={article.identifier}>
+                      <a href={articleRoute(article.identifier)}>
+                        {article.subtitle
+                          ? `${article.title} - ${article.subtitle}`
+                          : article.title}
+                      </a>
+                    </li>
+                  ))}
+              </ul>
+              <hr />
+              <h5>Socials</h5>
+              <ul>
+                <li>
+                  <a href="https://github.com/gautierblandin">GitHub</a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/in/gautier-blandin-661640138/">
+                    LinkedIn
+                  </a>
+                </li>
+              </ul>
+              <hr />
+              <h5>Contact</h5>
+              <p>
+                Feel free to reach out to me at:{' '}
+                <a href="mailto:gautier.blandin.dev@gmail.com">
+                  gautier.blandin.dev@gmail.com
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
     </NavbarLayout>
   );
 }
