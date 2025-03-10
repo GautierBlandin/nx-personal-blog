987c64906c60bace19c73407d8422407f0b38acd
Gautier <gautier.blandin@gmail.com>
2025-03-10 23:59:20 +0100

    feat(banner): remove banner from articles page

--- apps/personal-blog/src/app/app.tsx
+++ apps/personal-blog/src/app/app.tsx
@@ -3,25 +3,31 @@ import {
   ArticlePage,
   articleRoute,
   articles,
-  articleToArticlePageProps
+  articleToArticlePageProps,
 } from '@nx-personal-blog/web-articles';
 import { Home } from '@nx-personal-blog/home';
 
 export function App() {
-
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {...Object.values(articles).map((article) =>
-        <Route path={articleRoute(article.identifier)} element={<ArticlePage
-        {...articleToArticlePageProps(article)}
-        />} />)}
-      <Route path="/articles" element={<ArticlePage
-        articleLink="/content/articles.md"
-        title="Gautier Blandin - A blog about software engineering"
-        description="Gautier Blandin - A blog about software engineering"
-      />} />
+      {...Object.values(articles).map((article) => (
+        <Route
+          path={articleRoute(article.identifier)}
+          element={<ArticlePage {...articleToArticlePageProps(article)} />}
+        />
+      ))}
+      <Route
+        path="/articles"
+        element={
+          <ArticlePage
+            articleLink="/content/articles.md"
+            title="Gautier Blandin - A blog about software engineering"
+            description="Gautier Blandin - A blog about software engineering"
+            noBanner
+          />
+        }
+      />
     </Routes>
   );
 }
--- libs/web/articles/src/lib/ArticlePage.tsx
+++ libs/web/articles/src/lib/ArticlePage.tsx
@@ -8,17 +8,19 @@ export interface ArticlePageProps {
   title: string;
   description: string;
   subtitle?: string;
+  noBanner?: boolean;
 }
 
 export const ArticlePage = ({
-  articleLink,
   title,
+  articleLink,
   description,
   subtitle,
+  noBanner,
 }: ArticlePageProps) => {
   return (
     <NavbarLayout title={title} description={description}>
-      <Banner title={title} subtitle={subtitle} />
+      {!noBanner && <Banner title={title} subtitle={subtitle} />}
       <main className="flex-grow">
         <div className="flex justify-center mx-4">
           <div className="max-w-6xl w-full">
