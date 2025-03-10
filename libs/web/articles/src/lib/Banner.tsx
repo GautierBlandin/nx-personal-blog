27b8b0fa45a68cdf08c2fc2cbca218da075a682d
Gautier <gautier.blandin@gmail.com>
2025-03-10 23:30:06 +0100

    feat(banner): make banned fixed height

--- libs/web/articles/src/lib/Banner.tsx
+++ libs/web/articles/src/lib/Banner.tsx
@@ -7,10 +7,12 @@ interface BannerProps {
 
 export const Banner: React.FC<BannerProps> = ({ title, subtitle }) => {
   return (
-    <div className="w-full bg-neutral-800 text-white py-32 mb-16 shadow-[0_6px_10px_-2px_rgba(0,0,0,0.4)]">
-      <div className="mx-auto max-w-8xl">
-        <h1 className="text-white py-0">{title}</h1>
-        {subtitle && <h3 className="text-white">{subtitle}</h3>}
+    <div className="w-full bg-neutral-800 text-white h-64 mb-16 shadow-[0_6px_10px_-2px_rgba(0,0,0,0.4)] flex items-center">
+      <div className="mx-auto max-w-8xl w-full">
+        <div className="flex flex-col justify-center">
+          <h1 className="text-white py-0">{title}</h1>
+          {subtitle && <h3 className="text-white">{subtitle}</h3>}
+        </div>
       </div>
     </div>
   );
