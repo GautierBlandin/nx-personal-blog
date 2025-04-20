6e8520b82d5afda1c103ce2806a1808534a540b5
Gautier <gautier.blandin@gmail.com>
2025-04-20 19:19:13 +0200

    feat: remove defunct storybook host lib

---
+++ docs/design-docs/move-to-remix.md
@@ -0,0 +1,5 @@
+# Move to remix
+
+## Context
+
+I want to move my blog to Remix in order to leverage SSR.
--- libs/web/storybook-host/src/lib/storybook-container.tsx
+++
@@ -1,7 +0,0 @@
-import type { Decorator } from '@storybook/react';
-
-export const withContainer: Decorator = (Story) => (
-  <div style={{ maxWidth: '800px', margin: '0 auto', padding: '20px' }}>
-    <Story />
-  </div>
-);
