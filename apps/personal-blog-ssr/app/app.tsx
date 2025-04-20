acfb4924788b1e41ef70203c3815bd1fbf7d1edf
Gautier <gautier.blandin@gmail.com>
2025-04-20 23:44:24 +0200

    feat: add barebone ssr react-router app

---
+++ apps/personal-blog-ssr/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@nx-personal-blog/personal-blog-ssr" />
+    </div>
+  );
+}
+
+export default App;
